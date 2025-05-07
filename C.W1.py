6a9de5c6970fb0f60f295be7a0994ef31b188dda
rishikisgood <Kaulrishik15@gmail.com>
2025-05-07 16:44:09 +0530

    Add files via upload

---
+++ C.W1.py
@@ -0,0 +1,12 @@
+Tree1=100
+Tree2=123
+Tree3=98
+Tree4=54
+Tree5=76
+
+Sum=Tree1+Tree2+Tree3+Tree4+Tree5
+print("The sum of all the 5 Trees are:", Sum)
+
+Average=Sum/5
+print("The Average of all the Trees are:", Average)
+
